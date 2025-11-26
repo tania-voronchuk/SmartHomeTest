346638b3c56eb76c47c3290cf030ab2eb0d91b4f
Tania Voronchuk <tanya.perfekt.pr@gmail.com>
2025-11-26 17:20:03 +0200

    added led blink and pwm examples

--- examples/04PWM/04PWM.ino
+++ examples/04PWM/04PWM.ino
@@ -8,7 +8,7 @@ void loop () {
 for (int value = 0; value<255; value = value + 1) {
  analogWrite (ledPin, value); 
  delay (5); 
- }
+}
 
  for (int value = 255; value>0; value = value-1) {
  analogWrite (ledPin, value); 
